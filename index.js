3d2e95cb07e13d6c8a4d7e67a0eb8614a129fab3
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-24 21:21:49 -0500

    rainbow band

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 //npm i "package name"
 //create gitignore to ignore node modules from going to github
 
-
-
-console.log('test')
+const length = +process.argv[2]
+// const {chalkAnimation} = require('chalk-animation')
+// const {generateSlug} = require('random-word-slugs')
+import chalkAnimation from "chalk-animation"
+import { generateSlug } from "random-word-slugs"
+const bandName = generateSlug(length, {format: 'title'})
+chalkAnimation.rainbow(bandName)
